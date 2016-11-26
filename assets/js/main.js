1c4d362b9edf3f802f34b36cddf6c155acc9fd6b
ddutchie <ddutchie@gmail.com>
2016-11-26 15:27:10 +0200

    More IMages

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,10 @@
 							'images/bg01.jpg': 'center',
 							'images/bg02.jpg': 'center',
 							'images/bg03.jpg': 'center'
+							'images/bg04.jpg': 'center'
+							'images/bg05.jpg': 'center'
+							'images/bg06.jpg': 'center'
+							'images/bg07.jpg': 'center'
 						},
 
 					// Delay.
