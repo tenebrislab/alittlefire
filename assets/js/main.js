8e4011d256a3354a1dc526a258e1d2988c7a58fa
ddutchie <ddutchie@gmail.com>
2016-11-26 15:31:14 +0200

    More images

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,10 +41,10 @@
 						images: {
 							'images/bg01.jpg': 'center',
 							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
-							'images/bg04.jpg': 'center'
-							'images/bg05.jpg': 'center'
-							'images/bg06.jpg': 'center'
+							'images/bg03.jpg': 'center',
+							'images/bg04.jpg': 'center',
+							'images/bg05.jpg': 'center',
+							'images/bg06.jpg': 'center',
 							'images/bg07.jpg': 'center'
 						},
 
